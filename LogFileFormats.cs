349f9abca7f438a31947006841f84f8a0f938ea1
halmaia <halmaia@gmail.com>
2020-07-17 23:29:08 +0200

    Update LogFileFormats.cs

--- LogFileFormats.cs
+++ LogFileFormats.cs
@@ -32,6 +32,7 @@ namespace LowranceReader2
     #region Argument checks
     internal static class ArgumentChecks
     {
+        [ContractArgumentValidator]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void CheckArguments(in byte[] buffer, int offset, int size)
         {
@@ -40,7 +41,6 @@ namespace LowranceReader2
             const string BufferIsNull = "The ‘" + NameOfBuffer + "’ argument can not be null.";
             const string NameOfOffset = nameof(offset);
             const string WrongOffsetParameter = "Wrong offset parameter.";
-            string WrongBufferSize = "The length of the ‘" + NameOfBuffer + "’ argument must be equal to or longer than " + size.ToString() + '.';
             #endregion "Constants"
 
             #region "Argument checks"
@@ -53,11 +53,15 @@ namespace LowranceReader2
             int bufferLength = buffer.Length;
 
             if (bufferLength < size)
+            {
+                string WrongBufferSize = "The length of the ‘" + NameOfBuffer + "’ argument must be equal to or longer than " + size.ToString() + '.';
                 throw new ArgumentOutOfRangeException(NameOfBuffer, WrongBufferSize);
+            }
 
             if (bufferLength - offset < size)
                 throw new ArgumentException(WrongOffsetParameter, NameOfOffset);
-            #endregion "Argument checks" 
+            #endregion "Argument checks"
+            Contract.EndContractBlock();
         }
     }
     #endregion Argument checks
@@ -126,59 +130,62 @@ namespace LowranceReader2
         public bool IsHeadingValid => (ByteStore & 0B1000000000000000) != 0;
     }
 
-    [StructLayout(LayoutKind.Sequential, Size = Size)]
+    [StructLayout(LayoutKind.Explicit, Size = Size)]
     [DebuggerDisplay("{ToString()}")]
     public readonly struct SL3Frame
     {
         public const int Size = 168;
 
-        public uint PositionOfFirstByte { get; } // 0 --> 3
-        public uint Unknown1 { get; } // 4 --> 7
-        public ushort TotalLength { get; } // 8 --> 9
-        public ushort PreviousLength { get; } // 10 --> 11
-        public SurveyType SurveyType { get; } // 12 --> 13
-        public ushort Unknown2 { get; } // 14 --> 15
-        public uint NumberOfCampaignInThisType { get; } // 16 --> 19
-        public float MinRange { get; }  // 20 --> 23
-        public float MaxRange { get; } // 24 --> 27
-        public int Unknown3 { get; } // 28 --> 31
-        public int Unknown4 { get; } // 32 --> 35
-        public int Unknown5 { get; } // 33 --> 39
-        public uint HardwareTime { get; } // 40 --> 43
-        public uint OriginalLengthOfEchoData { get; } // 44 --> 47
-        public float WaterDepth { get; } // 48 --> 51
-        public Frequency Frequency { get; } // 52 --> 53
-        public int Unknown6 { get; } // 54 --> 57
-        public int Unknown7 { get; } // 58 --> 61
-        public int Unknown8 { get; } // 62 --> 65
-        public int Unknown9 { get; } // 66 --> 69
-        public int Unknown10 { get; } // 70 --> 73
-        public int Unknown11 { get; } // 74 --> 77
-        public int Unknown12 { get; } // 78 --> 81
-        public short Unknown13 { get; } // 82 --> 83
-        public float GNSSSpeed { get; } // 84 --> 87
-        public float WaterTemperature { get; } // 88 --> 91
-        public int X { get; } // 92 --> 95
-        public int Y { get; } // 96 --> 99
-        public float WaterSpeed { get; } // 100 --> 103
-        public float GNSSHeading { get; } // 104 --> 107
-        public float GNSSAltitude { get; } // 108 --> 111
-        public float MagneticHeading { get; } // 112 --> 115
-        public FrameFlags Flags { get; } // 116 --> 117
-        public int Unknown14 { get; } // 118 --> 121
-        public short Unknown15 { get; } // 122 --> 123
-        public uint Milliseconds { get; } //(124)
-        public uint LastPrimaryChannelFrameOffset { get; }  //(128)
-        public uint LastSecondaryChannelFrameOffset { get; }  //(132)
-        public uint LastDownScanChannelFrameOffset { get; }  //(136)
-        public uint LastSidescanLeftChannelFrameOffset { get; }  //(140)
-        public uint LastSidescanRightChannelFrameOffset { get; }  //(144)
-        public uint LastSidescanCompositeChannelFrameOffset { get; }  //(148)
-        public int Unknown16 { get; } // 152 --> 155
-        public int Unknown17 { get; } // 156 --> 159
-        public int Unknown18 { get; } // 160 --> 163
-        public uint Last3DChannelFrameOffset { get; }  //(164)
+        // Field offset attribute makes able us to comment out
+        // unused & unknown properties.
+        [field: FieldOffset(0)] public uint PositionOfFirstByte { get; } // 0 --> 3
+        [field: FieldOffset(4)] public uint Unknown1 { get; } // 4 --> 7
+        [field: FieldOffset(8)] public ushort TotalLength { get; } // 8 --> 9
+        [field: FieldOffset(10)] public ushort PreviousLength { get; } // 10 --> 11
+        [field: FieldOffset(12)] public SurveyType SurveyType { get; } // 12 --> 13
+        [field: FieldOffset(14)] public ushort Unknown2 { get; } // 14 --> 15
+        [field: FieldOffset(16)] public uint NumberOfCampaignInThisType { get; } // 16 --> 19
+        [field: FieldOffset(20)] public float MinRange { get; }  // 20 --> 23
+        [field: FieldOffset(24)] public float MaxRange { get; } // 24 --> 27
+        [field: FieldOffset(28)] public int Unknown3 { get; } // 28 --> 31
+        [field: FieldOffset(32)] public int Unknown4 { get; } // 32 --> 35
+        [field: FieldOffset(36)] public int Unknown5 { get; } // 36 --> 39
+        [field: FieldOffset(40)] public uint HardwareTime { get; } // 40 --> 43
+        [field: FieldOffset(44)] public uint OriginalLengthOfEchoData { get; } // 44 --> 47
+        [field: FieldOffset(48)] public float WaterDepth { get; } // 48 --> 51
+        [field: FieldOffset(52)] public Frequency Frequency { get; } // 52 --> 53
+        [field: FieldOffset(54)] public int Unknown6 { get; } // 54 --> 57
+        [field: FieldOffset(58)] public int Unknown7 { get; } // 58 --> 61
+        [field: FieldOffset(62)] public int Unknown8 { get; } // 62 --> 65
+        [field: FieldOffset(66)] public int Unknown9 { get; } // 66 --> 69
+        [field: FieldOffset(70)] public int Unknown10 { get; } // 70 --> 73
+        [field: FieldOffset(74)] public int Unknown11 { get; } // 74 --> 77
+        [field: FieldOffset(78)] public int Unknown12 { get; } // 78 --> 81
+        [field: FieldOffset(82)] public short Unknown13 { get; } // 82 --> 83
+        [field: FieldOffset(84)] public float GNSSSpeed { get; } // 84 --> 87
+        [field: FieldOffset(88)] public float WaterTemperature { get; } // 88 --> 91
+        [field: FieldOffset(92)] public int X { get; } // 92 --> 95
+        [field: FieldOffset(96)] public int Y { get; } // 96 --> 99
+        [field: FieldOffset(100)] public float WaterSpeed { get; } // 100 --> 103
+        [field: FieldOffset(104)] public float GNSSHeading { get; } // 104 --> 107
+        [field: FieldOffset(108)] public float GNSSAltitude { get; } // 108 --> 111
+        [field: FieldOffset(112)] public float MagneticHeading { get; } // 112 --> 115
+        [field: FieldOffset(116)] public FrameFlags Flags { get; } // 116 --> 117
+        [field: FieldOffset(118)] public int Unknown14 { get; } // 118 --> 121
+        [field: FieldOffset(122)] public short Unknown15 { get; } // 122 --> 123
+        [field: FieldOffset(124)] public uint Milliseconds { get; } //(124)
+        [field: FieldOffset(128)] public uint LastPrimaryChannelFrameOffset { get; }  //(128)
+        [field: FieldOffset(132)] public uint LastSecondaryChannelFrameOffset { get; }  //(132)
+        [field: FieldOffset(136)] public uint LastDownScanChannelFrameOffset { get; }  //(136)
+        [field: FieldOffset(140)] public uint LastSidescanLeftChannelFrameOffset { get; }  //(140)
+        [field: FieldOffset(144)] public uint LastSidescanRightChannelFrameOffset { get; }  //(144)
+        [field: FieldOffset(148)] public uint LastSidescanCompositeChannelFrameOffset { get; }  //(148)
+        [field: FieldOffset(152)] public uint UnknownOffset1 { get; } // 152 --> 155
+        [field: FieldOffset(156)] public uint UnknownOffset2 { get; } // 156 --> 159
+        [field: FieldOffset(160)] public uint UnknownOffset3 { get; } // 160 --> 163
+        [field: FieldOffset(164)] public uint Last3DChannelFrameOffset { get; }  //(164)
 
+        [SecuritySafeCritical()]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public unsafe SL3Frame(in byte[] buffer, int offset = 0, bool strictParameterCheck = true)
         {
