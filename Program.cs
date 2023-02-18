326ac734b1bf96bfb43006fd43318356ef8753b4
Ákos Halmai <halmaia@gmail.com>
2023-02-18 18:57:03 +0100

    Typo

--- Program.cs
+++ Program.cs
@@ -28,10 +28,10 @@ namespace SL3Reader
             }
 
             using SL3Reader sl3reader = new(input);
-            //sl3reader.ExamineUnknown8Datasets();
+
             sl3reader.Export3D(output);
             //sl3reader.ExportImagery(@"F:\SS\", SurveyType.DownScan);
-            
+            //sl3reader.ExamineUnknown8Datasets();
 
             //sl3reader.ExportToCSV(output);
         }
